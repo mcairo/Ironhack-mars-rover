f9d39a1728b66da8e0334e130c5c2abf481c17ca
mcairo <mcairo21057@columbushs.com>
2019-11-12 23:07:59 -0500

    rover

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,15 +1,80 @@
 // Rover Object Goes Here
 // ======================
-
+let rover = {
+  direction = 'N',
+  x: 0,
+  y: 0, 
+  travelLog = x,y 
+}
 // ======================
 function turnLeft(rover){
+  switch(rover.direction){
+    case 'N':
+      rover.direction = 'W';
+      break;
+    case 'W':
+      rover.direction = 'S';
+      break;
+    case 'S':
+      rover.direction = 'E';
+      break;
+    case 'E':
+      rover.direction = 'N';
+      break;
+        
+  }
   console.log("turnLeft was called!");
 }
 
 function turnRight(rover){
+  switch(rover.direction){
+    case 'N':
+      rover.direction = 'E';
+      break;
+    case 'E':
+      rover.direction = 'S';
+      break;
+    case 'S':
+      rover.direction = 'W';
+      break;
+    case 'W':
+      rover.direction = 'N';
+      break;
+  }
   console.log("turnRight was called!");
 }
 
 function moveForward(rover){
-  console.log("moveForward was called")
+  switch(rover.direction){
+    case 'N':
+      rover.y -= 1;
+      break;
+    case 'S':
+      rover.y += 1;
+      break;
+    case 'W':
+      rover.x -= 1;
+      break;
+    case 'E':
+      rover.x += 1;
+      break;
+
+  }
+  console.log(rover.x, rover.y)
+}
+
+
+function goForward(moves){
+  for (i = 0; i < moves.length; i++){
+    if(moves == 'r'){
+     return turnRight();
+    }
+    else if(moves == 'f'){
+      return moveForward();
+    }
+    else if(moves == 'l'){
+      return turnLeft();
+    }
+    console.log(rover.travelLog)
+  }
 }
